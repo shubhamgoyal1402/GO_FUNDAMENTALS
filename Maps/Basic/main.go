167d348e3359fa26e2201a408918354f4d4a222e
shubham goyal <shubhamgoyal1402@gmail.com>
2024-03-05 01:21:18 +0530

    Update main.go

--- Maps/Basic/main.go
+++ Maps/Basic/main.go
@@ -6,7 +6,7 @@ func main() {
 
 	names := []string{"ram", "sham", "raju", "pratham", "kiran"}
 	ages := []int{40, 25, 18, 43, 26}
-
+	// map structure : map_name := map[key_datatype]value_datatype{}
 	details := map[string]int{} //declaration of a map
 	for i := 0; i < len(names); i++ {
 		details[names[i]] = ages[i] // initialzing a map

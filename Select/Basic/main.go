ce9abe88518f662ed057e33d6b29d28030bafa45
shubham goyal <shubhamgoyal1402@gmail.com>
2024-03-12 21:59:00 +0530

    Create main.go

---
+++ Select/Basic/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	ninja1, ninja2 := make(chan string), make(chan string)
+
+	go attack(ninja1, "Ninja 1")
+	go attack(ninja2, "Ninja 2")
+
+	select {
+
+	case msg := <-ninja1:
+		fmt.Println(msg)
+
+	case msg := <-ninja2:
+		fmt.Println(msg)
+	}
+
+}
+
+func attack(val chan string, msg string) {
+	val <- msg
+}

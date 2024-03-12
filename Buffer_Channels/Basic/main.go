33d7ef9c07413e488a248b8982b65844259c79bc
shubham goyal <shubhamgoyal1402@gmail.com>
2024-03-12 12:01:46 +0530

    Create main.go

---
+++ Buffer_Channels/Basic/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+/*func main() {
+
+	val := make(chan string)
+
+	val <- "Shubham" // this will create deadlock cause the deafult capacity of channel is 0 and it cannot hold value
+	fmt.Println(<-val)
+
+}*/
+
+func main() {
+
+	val := make(chan string, 1) // make(chan TYPE, LENGTH)
+
+	val <- "Shubham" // it will hold the value
+	//val <- "NMikhil" // this will again create deadlock
+	fmt.Println(<-val)
+
+	val <- "Nikhil"
+	fmt.Println(<-val)
+}
